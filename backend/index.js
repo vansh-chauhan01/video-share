8d3fc51732b480450d9b7eb51e8a1cd0e11d70f2
vansh-chauhan01 <98vansh98@gmail.com>
2026-07-01 17:43:53 +0530

    Configure backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,11 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
